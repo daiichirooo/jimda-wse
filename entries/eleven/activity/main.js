7a82e1a2e35fc23dea86faa43342b2d26fa47e45
jimda328 <2024.daiichiro.nakanishi@uwcisak.jp>
2025-11-25 19:32:19 -0500

    class work

--- entries/eleven/activity/main.js
+++ entries/eleven/activity/main.js
@@ -5,3 +5,11 @@ let headlines = document.querySelectorAll("h1");
 
 console.log(headline);
 console.log(headlines);
+
+
+function addRed() {
+  headline.classList.toggle("red");
+}
+
+headline.addEventListener("click", addRed);
+
